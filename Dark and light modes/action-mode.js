7590876cff9b581bf060499ea95b7612f4e67837
flaluisaffm <flaluisaffm@gmail.com>
2025-10-16 13:08:42 +0200

    Updated

--- Dark and light modes/action-mode.js
+++ Dark and light modes/action-mode.js
@@ -4,11 +4,18 @@ const elementBody = document.body;
 
 //When I'm on the dark mode, it changes to light mode
 //Prove that the class dark-mode exists
-function changeText(){
+function changeMode(){
     const isOnDarkMode = elementBody.classList.contains("dark-mode");
-    selectorButton.textContent = isOnDarkMode ? "Change it to light mode &#9728" : "chnage it to dark mode &#127769"
-    // ? it is a bolean for if else statement, if is on dark mode, switch it to light mode and vice and versa
-    //&#9728 a sun icon
-    
-
+    selectorButton.textContent = isOnDarkMode ? "Change it to light mode ☀️" : "chnage it to dark mode 🌙"
+    // ? it is a boolean for if else statement, if is on dark mode, switch it to light mode and vice and versa
+    //&#9728 a sun icon    
 } 
+
+function changeTheme(){
+    elementBody.classList.toggle("dark-mode");
+    //toggle pass from on mode to another
+    changeMode();
+}
+selectorButton.addEventListener("click", changeMode);
+changeMode();
+
