7cf9da3a614ded6cf3a535ab63a0e3b9af3f7ad8
flaluisaffm <flaluisaffm@gmail.com>
2025-10-24 12:38:25 +0200

    Debugging complete

--- To do list/todo.js
+++ To do list/todo.js
@@ -1,26 +1,26 @@
 //SELECTORS to fetch the elements of DOM
 
-const { createElement } = require("react");
+//const { createElement } = require("react");
 
 //html --> list-input
 const listInput = document.getElementById("list-input");
 //html --> addList
 const addButton = document.getElementById("addList");
 //html --> <ul> toDoList
-const listToDo = document.getElementById("toDoList");
+const listToDoUl = document.getElementById("toDoList");
 
 //Relevant Events
 //DOMContentLoaded load everything from html
 document.addEventListener("DOMContentLoaded", loadLists); 
-addButton.addEventListener("click", toDoList);
+addButton.addEventListener("click", addNewtask);
 
 //Functions - logic of the app
 //before I had functin addNewList
-function toDoList(){
+function addNewtask(){
     //value means when something input is add in the field
     const toDoText = listInput.value.trim();
     if(toDoText){
-        createElementToDo(toDoText);
+        createElement(toDoText);
         saveToDo(toDoText);
         listInput.value = "";
     }
@@ -29,7 +29,7 @@ function toDoList(){
 
 //Main function to create elements <li> and its buttons
 //isCompleted has default value as False, otherwise all the to do list will be created as completed
-function createElementToDoList(toDoText, isCompleted = false){
+function createElement(toDoText, isCompleted = false){
     const li = document.createElement("li");
     li.className="task-item";
     //created data-text to identify as data
@@ -39,12 +39,12 @@ function createElementToDoList(toDoText, isCompleted = false){
     li.innerHTML = 
        `<span>${toDoText}</span>
         <button class="completed-btn">✓</button>
-        <button class="delete-btn"></button>`;
-    li.querySelector("completed-btn").addEventListener("click", completeList);
-    li.querySelector("delete-btn").addEventListener("click", deleteList);
+        <button class="delete-btn">🗑️</button>`;
+    li.querySelector(".completed-btn").addEventListener("click", completeList);
+    li.querySelector(".delete-btn").addEventListener("click", deleteList);
 
     //creating the son li and agregating to the list
-    listToDo.appendChild(li);
+    listToDoUl.appendChild(li);
 }
 
 function completeList(event){
@@ -52,45 +52,41 @@ function completeList(event){
     li.classList.toggle("completed");
 
     //Atualizar o estado da to do list
-    refreshToDoList(li.getAtribute("data-text"));
+    refreshlistToDoUl(li.getAttribute("data-text"));
 }
 function deleteList(event){
     const li = event.target.parentElement; 
-    const listText = li.getAtribute("data-text");
+    const listText = li.getAttribute("data-text");
 
-    listToDo.removeChild(li);
-    deleteList(listText);
+    listToDoUl.removeChild(li);
+    deleteTaskFromStorage(listText);
 }
 const obtainList = () => JSON.parse(localStorage.getItem('tasks')) || [];
 
 function loadLists(){
-    obtainList().forEach(task => createElementToDoList(task.text, task.completed));
+    obtainList().forEach(task => createElement(task.text, task.completed));
 
 }
 
 function saveToDo(listText){
     const tasks = obtainList();
     tasks.push({text:listText, completed:false});
-    localStorage.getItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function refreshToDoList(listText){
+function refreshlistToDoUl(listText){
     let tasks = obtainList();
 
     tasks = tasks.map(task =>
-        task.text == listText
+        task.text === listText
         ? { ...task, completed:!task.completed} //change its state
         : task //Mantain the remaining tasks the same
     );
     localStorage.setItem('tasks', JSON.stringify(tasks));
 
 }
-function deleteList(listText){
+function deleteTaskFromStorage(listText){
     //we used 'filter'to create a new array without the deleted task
     const tasks = obtainList().filter(task => task.text !== listText);
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-
-
-
-
