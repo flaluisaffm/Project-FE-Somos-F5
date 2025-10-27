25c83af4fa5949628fd9437da941fe4d472cb46a
flaluisaffm <flaluisaffm@gmail.com>
2025-10-27 22:25:47 +0100

    Fixes on pildora js

--- Pildora/pildora.js
+++ Pildora/pildora.js
@@ -12,8 +12,26 @@ password.addEventListener('input',() => {
   console.log(`Your password is ${password.value}`)
 });
 
-//function that will open an img when button in clicked
-function openExternalLink(event){
-  window.location.href = "https://cdn.pixabay.com/photo/2020/03/31/19/20/dog-4988985_1280.jpg"
+            
+const form = document.getElementById('form');
+if (!form) {
+  console.error('Form element #login-form not found.');
 }
+
+form.addEventListener('submit', (e) => {
+  // If the form is invalid, the submit event DOES NOT fire.
+  // So this handler only runs when the form is valid.
+
+  e.preventDefault(); // stop real POST to /login
+
+  console.log('Form valid. Redirecting...');
+  // Choose ONE of these:
+  // location.href = 'http://127.0.0.1:5500/Menu%20Navigation/index.html';
+  window.location.assign('http://127.0.0.1:5500/Menu%20Navigation/index.html');
+});
+
+
+
+
+
 
